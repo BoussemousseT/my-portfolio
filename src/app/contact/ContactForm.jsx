2109acbbfd672185f5a2249f5032da337b0495b6
Taoufik Boussemousse <114195598+BoussemousseT@users.noreply.github.com>
2025-06-18 20:21:15 -0400

    id

--- src/app/contact/ContactForm.jsx
+++ src/app/contact/ContactForm.jsx
@@ -46,7 +46,7 @@ const ContactForm = () => {
   const onSubmit = async (formData) => {
     setIsLoading(true);
     formData["interested-in"] = interestedIn;
-    const formSubmitURL = `https://fabform.io/f/sIABXUh`;
+    const formSubmitURL = `https://fabform.io/f/o6QgCvx`;
     try {
       const response = await fetch(formSubmitURL, {
         method: "POST",
